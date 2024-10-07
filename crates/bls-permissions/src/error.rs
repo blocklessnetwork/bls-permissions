b50add7a06ea2af1e348d970cf4f0827339c64b7
Join.G <joinhack@gmail.com>
2024-10-07 22:04:37 +0800

    init

---
+++ crates/bls-permissions/src/error.rs
@@ -0,0 +1,78 @@
+use std::{
+    borrow::Cow,
+    fmt::{self, Display, Formatter},
+};
+
+use anyhow::Error;
+
+/// Creates a new error with a caller-specified error class name and message.
+pub fn custom_error(class: &'static str, message: impl Into<Cow<'static, str>>) -> Error {
+    CustomError {
+        class,
+        message: message.into(),
+    }
+    .into()
+}
+
+pub fn generic_error(message: impl Into<Cow<'static, str>>) -> Error {
+    custom_error("Error", message)
+}
+
+pub fn type_error(message: impl Into<Cow<'static, str>>) -> Error {
+    custom_error("TypeError", message)
+}
+
+pub fn range_error(message: impl Into<Cow<'static, str>>) -> Error {
+    custom_error("RangeError", message)
+}
+
+pub fn invalid_hostname(hostname: &str) -> Error {
+    type_error(format!("Invalid hostname: '{hostname}'"))
+}
+
+pub fn uri_error(message: impl Into<Cow<'static, str>>) -> Error {
+    custom_error("URIError", message)
+}
+
+pub fn bad_resource(message: impl Into<Cow<'static, str>>) -> Error {
+    custom_error("BadResource", message)
+}
+
+pub fn bad_resource_id() -> Error {
+    custom_error("BadResource", "Bad resource ID")
+}
+
+pub fn not_supported() -> Error {
+    custom_error("NotSupported", "The operation is not supported")
+}
+
+pub fn resource_unavailable() -> Error {
+    custom_error(
+        "Busy",
+        "Resource is unavailable because it is in use by a promise",
+    )
+}
+
+/// A simple error type that lets the creator specify both the error message and
+/// the error class name. This type is private; externally it only ever appears
+/// wrapped in an `anyhow::Error`. To retrieve the error class name from a wrapped
+/// `CustomError`, use the function `get_custom_error_class()`.
+#[derive(Debug)]
+struct CustomError {
+    class: &'static str,
+    message: Cow<'static, str>,
+}
+
+impl Display for CustomError {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        f.write_str(&self.message)
+    }
+}
+
+impl std::error::Error for CustomError {}
+
+/// If this error was crated with `custom_error()`, return the specified error
+/// class name. In all other cases this function returns `None`.
+pub fn get_custom_error_class(error: &Error) -> Option<&'static str> {
+    error.downcast_ref::<CustomError>().map(|e| e.class)
+}
---
+++ crates/bls-permissions/src/prompter.rs
@@ -0,0 +1,67 @@
+use once_cell::sync::Lazy;
+use parking_lot::Mutex;
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum PromptResponse {
+    Allow,
+    Deny,
+    AllowAll,
+}
+
+pub type PromptCallback = Box<dyn FnMut() + Send + Sync>;
+
+pub trait PermissionPrompter: Send + Sync {
+    fn prompt(
+        &mut self,
+        message: &str,
+        name: &str,
+        api_name: Option<&str>,
+        is_unary: bool,
+    ) -> PromptResponse;
+}
+
+struct AllowPrompter;
+
+impl PermissionPrompter for AllowPrompter {
+    fn prompt(
+        &mut self,
+        _message: &str,
+        _name: &str,
+        _api_name: Option<&str>,
+        _is_unary: bool,
+    ) -> PromptResponse {
+        PromptResponse::AllowAll
+    }
+}
+
+static PERMISSION_PROMPTER: Lazy<Mutex<Box<dyn PermissionPrompter>>> =
+    Lazy::new(|| Mutex::new(Box::new(AllowPrompter)));
+
+static MAYBE_BEFORE_PROMPT_CALLBACK: Lazy<Mutex<Option<PromptCallback>>> =
+    Lazy::new(|| Mutex::new(None));
+
+static MAYBE_AFTER_PROMPT_CALLBACK: Lazy<Mutex<Option<PromptCallback>>> =
+    Lazy::new(|| Mutex::new(None));
+
+pub fn permission_prompt(
+    message: &str,
+    flag: &str,
+    api_name: Option<&str>,
+    is_unary: bool,
+) -> PromptResponse {
+    if let Some(before_callback) = MAYBE_BEFORE_PROMPT_CALLBACK.lock().as_mut() {
+        before_callback();
+    }
+    let r = PERMISSION_PROMPTER
+        .lock()
+        .prompt(message, flag, api_name, is_unary);
+    if let Some(after_callback) = MAYBE_AFTER_PROMPT_CALLBACK.lock().as_mut() {
+        after_callback();
+    }
+    r
+}
+
+pub fn set_prompt_callbacks(before_callback: PromptCallback, after_callback: PromptCallback) {
+    *MAYBE_BEFORE_PROMPT_CALLBACK.lock() = Some(before_callback);
+    *MAYBE_AFTER_PROMPT_CALLBACK.lock() = Some(after_callback);
+}
